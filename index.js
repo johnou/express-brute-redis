f99ff36c94d5efaf4178b581e9d8adec2da021b6
Johno Crawford <johno@sulake.com>
2023-11-15 15:39:58 +0100

    Use redis counter to avoid rate limiting bypass.

--- index.js
+++ index.js
@@ -25,32 +25,40 @@ RedisStore.prototype = Object.create(AbstractClientStore.prototype);
 RedisStore.prototype.set = function (key, value, lifetime, callback) {
 	lifetime = parseInt(lifetime, 10) || 0;
 	var multi    = this.client.multi(),
-	    redisKey = this.options.prefix+key;
+	    redisKey = this.options.prefix+key,
+		counterKey = redisKey + ':counter';
 
 	multi.set(redisKey, JSON.stringify(value));
+	multi.incr(counterKey);
 	if (lifetime > 0) {
 		multi.expire(redisKey, lifetime);
+		multi.expire(counterKey, lifetime + 1);
 	}
 	multi.exec(function (err, data) {
 		typeof callback == 'function' && callback.call(this, null);
 	});
 };
 RedisStore.prototype.get = function (key, callback) {
-	this.client.get(this.options.prefix+key, function (err, data) {
+	var redisKey = this.options.prefix+key,
+		counterKey = redisKey + ':counter';
+	this.client.mGet([redisKey, counterKey], function (err, replies) {
 		if (err) {
 			typeof callback == 'function' && callback(err, null);
 		} else {
+			var data = Array.isArray(replies) ? JSON.parse(replies[0]) : null;
 			if (data) {
-				data = JSON.parse(data);
 				data.lastRequest = new Date(data.lastRequest);
 				data.firstRequest = new Date(data.firstRequest);
+				data.count = parseInt(replies[1], 10) || 0;
 			}
 			typeof callback == 'function' && callback(err, data);
 		}
 	});
 };
 RedisStore.prototype.reset = function (key, callback) {
-	this.client.del(this.options.prefix+key, function (err, data) {
+	var redisKey = this.options.prefix + key,
+		counterKey = redisKey + ':counter';
+	this.client.del([redisKey, counterKey], function (err) {
 		typeof callback == 'function' && callback.apply(this, arguments);
 	});
 };
